b8708fa77f35d00613bd62bbc0d3d9ab01ca1010
Samantha Reis <smthalvesreis@gmail.com>
2021-10-08 01:01:59 -0300

    Lista de exercícios - POO

---
+++ src/aplicacao/Teste.java
@@ -0,0 +1,91 @@
+package aplicacao;
+
+import java.util.Locale;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		
+		// criação do novo objeto Locale
+		Locale.setDefault(Locale.US);
+		
+		//Instanciando 4 empregados da classe Empregado
+		//E chamando o construtor com os argumentos e atribuindo valores respectivamente;
+		Empregado empregado1 = new Empregado("Joao", 9, 36);
+		Empregado empregado2 = new Empregado("Maria", 9, 56);
+		Empregado empregado3 = new Empregado("Pedro", 9, 76);
+		Empregado empregado4 = new Empregado("Ana", 9, 45);
+		
+		
+        // Estrutura condicional composta 01
+		if (empregado1.calculaSalarioMensal() > empregado2.calculaSalarioMensal()
+				&& empregado1.calculaSalarioMensal() > empregado3.calculaSalarioMensal()
+				&& empregado1.calculaSalarioMensal() > empregado4.calculaSalarioMensal()) {
+			System.out.printf("O empregado 1 de nome %s trabalhou %d horas e tem o maior salario. Valor: R$ %.2f%n",
+					empregado1.getNome(), empregado1.getHorasTrabalhadasNoMes(), empregado1.calculaSalarioMensal());
+		} else if (empregado2.calculaSalarioMensal() > empregado1.calculaSalarioMensal()
+				&& empregado2.calculaSalarioMensal() > empregado3.calculaSalarioMensal()
+				&& empregado2.calculaSalarioMensal() > empregado4.calculaSalarioMensal()) {
+			System.out.printf("O empregado 2 de nome %s trabalhou %d horas e tem o maior salario. Valor: R$ %.2f%n",
+					empregado2.getNome(), empregado2.getHorasTrabalhadasNoMes(), empregado2.calculaSalarioMensal());
+
+		} else if (empregado3.calculaSalarioMensal() > empregado1.calculaSalarioMensal()
+				&& empregado3.calculaSalarioMensal() > empregado2.calculaSalarioMensal()
+				&& empregado3.calculaSalarioMensal() > empregado4.calculaSalarioMensal()) {
+			System.out.printf("O empregado 3 de nome %s trabalhou %d horas e tem o maior salario. Valor: R$ %.2f%n",
+					empregado3.getNome(), empregado3.getHorasTrabalhadasNoMes(), empregado3.calculaSalarioMensal());
+
+		} else {
+			System.out.printf("O empregado 4 de nome %s trabalhou %d horas tem o maior salario. Valor: R$ %.2f%n \n",
+					empregado4.getNome(), empregado4.getHorasTrabalhadasNoMes(), empregado4.calculaSalarioMensal());
+
+		} // Fim da estrutura condicional composta 01;
+		
+		// invocando método do cálculo da média salarial da classe Empregado;
+		float mediaSalarial = empregado1.mediaSalarial(empregado1, empregado2, empregado3, empregado4);
+		
+        // Listando salário dos empregados;
+		System.out.println("\nLista de salários dos empregados:");
+		System.out.printf("\nO empregado 1 - R$ %.2f%n", empregado1.calculaSalarioMensal());
+		System.out.printf("O empregado 2 - R$ %.2f%n", empregado2.calculaSalarioMensal());
+		System.out.printf("O empregado 3 - R$ %.2f%n", empregado3.calculaSalarioMensal());
+		System.out.printf("O empregado 4 - R$ %.2f%n", empregado4.calculaSalarioMensal());
+		
+       //Mostrar média do salário dos 4 empregados 
+		System.out.printf("\nMedia salarial dos empregados: R$ %.2f%n", mediaSalarial);
+		
+		
+      //Estrutura condicional composta 02 - para verificar salário(s) abaixo da média e aumentar em 20% os que estão abaixo;
+		if (empregado1.calculaSalarioMensal() < mediaSalarial) {
+			float aumento1 = empregado1.aumentoMediaSalarial(empregado1);
+
+			System.out.printf("\nO empregado %s teve um aumento de vinte por cento e seu novo salário é: %.2f%n",
+					empregado1.getNome(), aumento1);
+
+		}
+		if (empregado2.calculaSalarioMensal() < mediaSalarial) {
+			float aumento2 = empregado2.aumentoMediaSalarial(empregado2);
+
+			System.out.printf("\nO empregado %s teve um aumento de vinte por cento e seu novo salário é: %.2f%n",
+					empregado2.getNome(), aumento2);
+		}
+		if (empregado3.calculaSalarioMensal() < mediaSalarial) {
+			float aumento3 = empregado3.aumentoMediaSalarial(empregado3);
+
+			System.out.printf("\nO empregado %s teve um aumento de vinte por cento e seu novo salário é: %.2f%n",
+					empregado3.getNome(), aumento3);
+
+		}
+		if (empregado4.calculaSalarioMensal() < mediaSalarial) {
+			float aumento4 = empregado4.aumentoMediaSalarial(empregado4);
+
+			System.out.printf("\nO empregado %s teve um aumento de vinte por cento e seu novo salário é: %.2f%n",
+					empregado4.getNome(), aumento4);
+			
+			//fim
+
+		}
+
+	}
+}
---
+++ src/aplicacao/TesteMenu.java
@@ -0,0 +1,112 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteMenu {
+
+	public static void main(String[] args) {
+
+		// criação do novo objeto Locale
+		Locale.setDefault(Locale.US);
+
+		// instanciando novo objeto Scanner para entrada de dados;
+		Scanner leitor = new Scanner(System.in);
+
+		// declarando e inciando variável para receber opções do Menu;
+		int numOpcao = 0;
+
+		// instanciando novo empregado da Classe empregado
+		Empregado empregado = new Empregado();
+
+		// estrutura de repetição while (enquanto)
+		while (numOpcao != 5) {
+
+			System.out.println("Selecione a opção desejada: \n\n1. Criar empregado \n2. Calcular salário do mensal\n3. "
+					+ "Alterar horas trabalhadas no mês \n4. Alterar salário do empregado \n5. Sair.");
+
+			numOpcao = leitor.nextInt();
+			String opcao;
+
+			// Estrutura de decisão condicional:
+			// O condicional switch testa o valor contido em uma variável e o compara com os
+			// valores fornecidos em cada caso,
+			switch (numOpcao) {
+
+			case 1:
+				opcao = "Você escolheu a opção: Criar empregado";
+
+				System.out.println(opcao);
+
+				System.out.println("\nDigite seu nome: ");
+				String nome = leitor.next();
+
+				System.out.println("\nDigite as horas trabalhadas: ");
+				int horasTrabalhadasNoMes = leitor.nextInt();
+
+				System.out.println("\nDigite seu salario por hora: ");
+				float salarioPorHora = leitor.nextFloat();
+
+				empregado.setNome(nome);
+				empregado.setHorasTrabalhadasNoMes(horasTrabalhadasNoMes);
+				empregado.setSalarioPorHora(salarioPorHora);
+				System.out.println(empregado.toString());
+
+				break;
+				
+				//O comando "break", quando executado, encerra a execução da estrutura onde ele se encontra.
+
+			case 2:
+				opcao = "Você escolheu a opção: Calcular salario mensal";
+
+				System.out.println(opcao);
+				System.out.printf("Seu salário é: R$ %.2f%n", empregado.calculaSalarioMensal());
+
+				break;
+
+			case 3:
+				opcao = "Alterar horas trabalhadas no mês";
+				System.out.println("\nVocê escolheu: Alterar as horas trabalhadas no mês \n\nAltere: ");
+				horasTrabalhadasNoMes = leitor.nextInt();
+				empregado.setHorasTrabalhadasNoMes(horasTrabalhadasNoMes);
+				System.out
+						.println("Horas trabalhadas alteradas para " + empregado.getHorasTrabalhadasNoMes() + " horas");
+				System.out.printf("Seu novo salário é: R$ %.2f%n", empregado.calculaSalarioMensal());
+
+				break;
+
+			case 4:
+				opcao = "Alterar salario do empregado";
+				System.out.println("\nVocê escolheu: Alterar salario do empregado \n\nAltere: ");
+				salarioPorHora = leitor.nextFloat();
+				empregado.setSalarioPorHora(salarioPorHora);
+				System.out.println(
+						"Valor recebido por hora alterado para R$ " + empregado.getSalarioPorHora() + " reais");
+				System.out.printf("Seu novo salário é: R$ %.2f%n", empregado.calculaSalarioMensal());
+
+				break;
+
+			case 5:
+				opcao = "Sair";
+				System.out.println("Obrigado por utilizar o sistema!");
+
+				break;
+
+			default:
+				opcao = "Opção inválida";
+				break;
+
+			/*
+			 *  obs: Caso todos os casos sejam testados e nenhum deles corresponda ao valor
+			 * da variável de teste,será executado o bloco de código associado à seção
+			 * default (“padrão”) do condicional.
+			 */
+
+			}
+
+		} // fim
+
+		leitor.close();
+	}
+
+}
